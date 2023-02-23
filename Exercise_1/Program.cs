d5e33bc37a6589ae5a60090cccd81d0569ae501c
Camillar <camillacrew@mail.ru>
2023-02-23 18:26:51 +0400

    Добавили задачу Exercise_1

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,32 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Power(int powerBase, int extent)
+{
+    int power = 1;
+    for (int i = 0; i < extent; i++)
+    {
+        power *= powerBase;   
+    }
+    return power;
+}
+bool ValidateExtent(int extent)
+{
+    if (extent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите основное число: ");
+int extent = Prompt("Введите натуральную степень: ");
+if (ValidateExtent(extent))
+{
+    System.Console.WriteLine($"Число {powerBase} в стпени {extent} равно {Power(powerBase, extent)}");
+}
