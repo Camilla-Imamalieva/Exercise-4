b3efeb0cdb453fea0ed6681552f9b97a991af02e
Camillar <camillacrew@mail.ru>
2023-02-24 15:47:30 +0400

    Добавили задачу Exercise_2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {s} = {SumAllDigit(s)}");
+int result = 0;
+return result;
+
+int SumAllDigit(int s)
+{
+    int result = 0;
+    while (s > 0)
+    {
+        result += s % 10;
+        s = s / 10;
+    }
+    return result;
+}
